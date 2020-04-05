fec976ef11e2cadef02b28566d1efd43912b8230
Marcio Hiroyuki Miyamoto <marcio.miyamoto@engdb.com.br>
2020-04-05 04:18:06 -0300

    Criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -19,5 +21,12 @@ if __name__ == '__main__':
     julia.sobrenome='Masuno'
     del julia.filhos
     print(julia.sobrenome)
+    hiro.olhos=1
+    del hiro.olhos
+    Pessoa.olhos=3
     print(julia.__dict__)
     print(hiro.__dict__)
+    print(Pessoa.olhos)
+    print(julia.olhos)
+    print(hiro.olhos)
+    print(id(Pessoa.olhos), id(julia.olhos), id(hiro.olhos))
