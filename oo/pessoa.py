7a98c7d064c0912fa4a66dde67a85c7f17321a16
Marcio Hiroyuki Miyamoto <marcio.miyamoto@engdb.com.br>
2020-04-05 04:08:34 -0300

    Criado atributos de instâncias filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Marcio2')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome='Marcio'
-    print(p.nome)
-    print(p.idade)
+    hiro = Pessoa(nome='Hiro')
+    julia = Pessoa(hiro, nome='Julia')
+    print(Pessoa.cumprimentar(julia))
+    print(id(julia))
+    print(julia.cumprimentar())
+    print(julia.idade)
+    for filho in julia.filhos:
+        print(filho.nome)
