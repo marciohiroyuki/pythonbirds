1903e891d97ba9ebfae87977b92e412c13d6113f
Marcio Hiroyuki Miyamoto <marcio.miyamoto@engdb.com.br>
2020-04-06 20:28:50 -0300

    Finalização do PythonBirds.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,9 +17,22 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        return f'Aperto de mão'
+
+class Mulher(Pessoa):
+    pass
+
+class Mutante(Pessoa):
+    olhos = 3
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Sinal Spock.'
+
 if __name__ == '__main__':
-    hiro = Pessoa(nome='Hiro')
-    julia = Pessoa(hiro, nome='Julia')
+    hiro = Mutante(nome='Hiro')
+    julia = Mulher(hiro, nome='Julia')
     print(Pessoa.cumprimentar(julia))
     print(id(julia))
     print(julia.cumprimentar())
@@ -31,7 +44,6 @@ if __name__ == '__main__':
     print(julia.sobrenome)
     hiro.olhos=1
     del hiro.olhos
-    Pessoa.olhos=3
     print(julia.__dict__)
     print(hiro.__dict__)
     print(Pessoa.olhos)
@@ -40,3 +52,11 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(julia.olhos), id(hiro.olhos))
     print(Pessoa.metodo_estatico(), julia.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), julia.nome_e_atributos_de_classe())
+    p = Pessoa(nome='Anonimo')
+    print(isinstance(p, Pessoa))
+    print(isinstance(p, Homem))
+    print(isinstance(hiro, Pessoa))
+    print(isinstance(hiro, Homem))
+    print(hiro.olhos)
+    print(hiro.cumprimentar())
+    print(julia.cumprimentar())
